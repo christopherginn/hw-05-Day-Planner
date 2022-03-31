0f43b95f767dc47b27fa654aaacad6212c24817b
christopherginn <hey.im.chris@live.com>
2022-03-31 16:47:23 -0400

    Added blocks for the hours. Added text area for task description

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+// Set current day in jumbotron
 var currentDay = moment().format("dddd MMMM Do");
 
 $("#currentDay").text(currentDay);
+
+var hoursInDay = [
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM"
+]
+
+$.each(hoursInDay, function(index, value){
+    $(".time-block").append("<tr class='row'><td class='hour'>"+value+"</td><td class='description'></td><td><button class='saveBtn'>save</button></td></tr>");
+});
+
+$(".description").append("<textarea placeholder='enter task'></textarea>");
