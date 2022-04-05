27abffb677d3c015a3a23bb81a35b41b816b8c8d
christopherginn <hey.im.chris@live.com>
2022-04-04 21:10:37 -0400

    hour conditioning and localstorage set

--- script.js
+++ script.js
@@ -4,20 +4,48 @@ var currentDay = moment().format("dddd MMMM Do");
 
 $("#currentDay").text(currentDay);
 
-var hoursInDay = [
-    "9AM",
-    "10AM",
-    "11AM",
-    "12PM",
-    "1PM",
-    "2PM",
-    "3PM",
-    "4PM",
-    "5PM"
-]
 
-$.each(hoursInDay, function(index, value){
-//     $(".time-block").append("<div class='row time-block'><div class='hour col-2'>"+value+"</div><div class='description col-9'></div><div><button class='saveBtn col-1'>save</button></div></div>");
-// });
+var container = $(".container");
+var hours = ["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
+var dataHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+var savedTask
 
-// $(".description").append("<textarea placeholder='enter task'></textarea>");
+
+$.each(hours, function(index, hour){
+    // console.log(hour);
+
+    container.append(` 
+    <div class="row time-block">
+        <div id="hour${dataHours[index]}" class="col-md-1 hour" data-hour="${dataHours[index]}">${hour}</div>
+        <textarea id="text${dataHours[index]}" class="col-md-10 description" placeholder="Enter task(s)"></textarea>
+        <button id="btn${dataHours[index]}" class="col-md-1 btn saveBtn"><i class="fas fa-save"></i></button>
+    </div>`);
+
+    var time = dataHours[index];
+    var iD = $("#text"+time);
+    console.log(time);
+
+    if (time == moment().hour()){
+        iD.addClass("present");
+    } else if (time > moment().hour()){
+        iD.addClass("future");
+    } else {
+        iD.addClass("past");
+    };
+
+    var saveBtn = $("#btn"+time);
+    saveBtn.on("click", function(){
+        var userInput = $("#text"+time).val();
+        localStorage.setItem('task'+time, userInput);
+    });
+});
+
+// function saveTask(){
+//     saveBtn.on("click", function(){
+        
+//         localStorage.setItem('task'+time, iD.value);
+//     })
+// };
+// function getTask(){
+
+// };
